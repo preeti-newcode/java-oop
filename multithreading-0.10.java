8beeb5dff7388b1c21f77512cbe3af000f1b8f21
preeti-newcode <preetiraj823@gmail.com>
2025-05-17 20:49:31 +0530

    Create multithreading-0.10.java

---
+++ multithreading-0.10.java
@@ -0,0 +1,28 @@
+class My implements Runnable{
+    private int no;
+    My(int no){
+        this.no=no;
+    }
+    public void run(){
+        for(int a=0;no>=a;no--){
+            System.out.println(Thread.currentThread().getName()+" = "+no);
+            try{
+                Thread.sleep(2000);
+            }catch(Exception e){
+                
+            }
+        }
+    }
+}
+class Main{
+    public static void main(String[] args){
+        My th=new My(5);
+        My thh=new My(7);
+        Thread t=new Thread(th);
+        Thread t2=new Thread(thh);
+        
+        t.start();
+        t2.start();
+        
+    }
+}
