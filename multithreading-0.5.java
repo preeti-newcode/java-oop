1ab32b6bc046ffff52230869b2be9abd9258901b
preeti-newcode <preetiraj823@gmail.com>
2025-05-17 18:50:14 +0530

    Create multithreading-0.5.java

---
+++ multithreading-0.5.java
@@ -0,0 +1,23 @@
+class thread extends Thread{
+    public int value;
+    public void run(){
+        for(int a=0;a<5;a++){
+            System.out.println(value++);
+            try{
+                Thread.sleep(100);
+            }catch(Exception e){}
+        }
+    }
+}
+
+class Main{
+    public static void main(String[] args){
+        thread t=new thread();
+        t.value=2;
+        thread t2=new thread();
+        t2.value=100;
+        
+        t2.start();
+        t.start();
+    }
+}
